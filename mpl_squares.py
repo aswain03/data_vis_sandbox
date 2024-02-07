348f26c97551e9b8080538f786c95356f9d85f18
aswain03 <alexander.swain03@gmail.com>
2024-02-07 16:16:37 +0000

    adds labels, title and line thickness

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# title and labels
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# tick labels
+ax.tick_params(labelsize=14)
 
 plt.show()
