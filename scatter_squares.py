126d409547f0d86f05e94bcb849e264a61180b13
aswain03 <alexander.swain03@gmail.com>
2024-02-08 08:55:36 +0000

    adds colour map to y values

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # title and labels
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,5 +14,9 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # tick labels
 ax.tick_params(labelsize=14)
+
+# range for the axises(?)
+ax.axis([0, 1100, 0, 1_100_000])
+# ax.ticklabel_format(style="plain") <- can override the notation (doesnt look good with larger numbers)
 
 plt.show()
