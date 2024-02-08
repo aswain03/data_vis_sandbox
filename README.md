7d9bb6eacde7ce6aa0eb3744d54eed0459d97dbd
aswain03 <alexander.swain03@gmail.com>
2024-02-08 09:07:47 +0000

    saves plots to .png instead of opening a new window

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # data_vis_sandbox
 - making simple plots with matplotlib
+
+## Random Walk
+I'll be using python to generate some random data and using matplotlib to create a representation of that data. 
--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Greens, s=10)
 
 # title and labels
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,6 +17,11 @@ ax.tick_params(labelsize=14)
 
 # range for the axises(?)
 ax.axis([0, 1100, 0, 1_100_000])
-# ax.ticklabel_format(style="plain") <- can override the notation (doesnt look good with larger numbers)
+# ax.ticklabel_format(style="plain") <- can override the notation
+# (doesnt look good with larger numbers)
 
-plt.show()
+# opens a window showing the plot
+# plt.show()
+
+# saves plot to file
+plt.savefig("squares_plot.png", bbox_inches="tight")
